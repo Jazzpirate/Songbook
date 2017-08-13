ce9a95caab4d07e86f184954a0fe8f677773053e
Dennis Müller <d.mueller@jacobs-university.de>
2017-08-13 20:12:12 +0200

    Now generates both book and mobile version at once

--- build/src/build.scala
+++ build/src/build.scala
@@ -18,24 +18,46 @@ object Build {
     def latex = if (todo) "\\todoband{" + name + "}{" + id + "}" else "\\band{" + name + "}{" + id + "}"
 
   }
+
+  def clear: Unit = {
+    println("\033[H\033[2J")
+  }
+  val max = 40
+  def prl(s : String, newline : Boolean = false) = print("\r" + s + (0 to (max-s.length)).toList.map(_ => ' ').mkString("") +
+    {if (newline) "\n" else ""})
+
   def main(args: Array[String]): Unit = {
     def run(s : String*) = ShellCommand.runIn(File("."),s:_*)
 
-    val tf = File.read(File("./songbook.template"))
     val bandfiles = File("./bands/").children.filter(_.getExtension contains "tex")
     val bands = bandfiles.map(Band).sortBy(_.id)
+
+    val tf = File.read(File("./songbook.template")).replace("%%BANDS%%",
+      bands.map(_.latex).mkString("\n")
+    )
     ShellCommand.run("touch", "main.tex")
-    val (main,build) = ((File(".") / "main").addExtension("tex"),(File(".") / "build").addExtension("sh"))
+    ShellCommand.run("touch", "mobile.tex")
+    val (main,mobile) = (
+      (File(".") / "main").addExtension("tex"),
+      (File(".") / "mobile").addExtension("tex"))
 
-    File.write(main,tf.replace("%%BANDS%%",
-        bands.map(_.latex).mkString("\n")
-    ))
+    File.write(main,tf.replace("%%VERSION%%","\\book"))
+    File.write(mobile,tf.replace("%%VERSION%%","\\mobile"))
 
+    clear
+    prl("First run: main.pdf")
     run("pdflatex","main")
-    run("texlua","songidx.lua","cbmain.sxd", "cbmain.sbx")
-    run("texlua","songidx.lua","cbbands.sxd","cbbands.sbx")
-    bands.foreach(b => run("texlua","songidx.lua", "bands/" + b.id + ".sxd", "bands/" + b.id + ".sbx"))
+    prl("First run: mobile.pdf")
+    run("pdflatex","mobile")
+    prl("First run: Done.",true)
+    // run("texlua","songidx.lua","cbmain.sxd", "cbmain.sbx")
+    // run("texlua","songidx.lua","cbbands.sxd","cbbands.sbx")
+    // bands.foreach(b => run("texlua","songidx.lua", "bands/" + b.id + ".sxd", "bands/" + b.id + ".sbx"))
+    prl("Second run: main.pdf")
     run("pdflatex","main")
+    prl("Second run: mobile.pdf")
+    run("pdflatex","mobile")
+    prl("Second run: Done.",true)
   }
 
 }
